54645c744bfe3089ce8562d6520b7d4008ce1a48
vishnu <vishnumariappan259@gmail.com>
2025-03-16 18:29:56 +0530

    add file or folder basic

--- File-Explorer/src/Explorer.jsx
+++ File-Explorer/src/Explorer.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
+import { VscNewFile } from "react-icons/vsc";
+import { VscNewFolder } from "react-icons/vsc";
 import FileNode from "./FileNode";
+import "./Explorer.css";
 
 const Explorer = () => {
   const [fileStructure, setFileStructure] = useState([
@@ -35,10 +38,48 @@ const Explorer = () => {
     },
   ]);
 
+  const [activeTree, setActiveTree] = useState(null);
+
+  const onAddFile = () => {
+    if (activeTree) {
+    } else {
+      setFileStructure((preStruc) => [
+        ...preStruc,
+        {
+          id: nanoid(),
+          type: "file",
+          name: "New File",
+        },
+      ]);
+    }
+  };
+
+  const onAddFolder = () => {
+    if (activeTree) {
+    } else {
+      setFileStructure((preStruc) => [
+        ...preStruc,
+        {
+          id: nanoid(),
+          type: "folder",
+          name: "New Folder",
+        },
+      ]);
+    }
+  };
+
   return (
     <main className="explorer-main-container">
+      <section className="explorer-tools">
+        <span className="add-file-icon" onClick={onAddFile}>
+          <VscNewFile />
+        </span>
+        <span className="add-folder-icon" onClick={onAddFolder}>
+          <VscNewFolder />
+        </span>
+      </section>
       {fileStructure.map((node) => (
-        <FileNode key={node.id} node={node} />
+        <FileNode key={node.id} node={node} setActiveTree={setActiveTree} />
       ))}
     </main>
   );
--- File-Explorer/src/FileNode.jsx
+++ File-Explorer/src/FileNode.jsx
@@ -2,20 +2,28 @@ import React, { use, useState } from "react";
 import { FaFile, FaFolder, FaFolderOpen } from "react-icons/fa";
 import "./FileNode.css";
 
-const FileNode = ({ node, isChild = false }) => {
+const FileNode = ({ node, isChild = false, setActiveTree }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const toggleFolder = () => setIsOpen(!isOpen);
+  const toggleFolder = (id) => {
+    setIsOpen(!isOpen);
+    setActiveTree(id);
+  };
 
   return (
     <section className={`file-node-section ${isChild ? "child" : ""}`}>
       {node.type === "folder" ? (
         <div className="folder-section">
-          <span onClick={toggleFolder} className="folder">
+          <span onClick={() => toggleFolder(node.id)} className="folder">
             {isOpen ? <FaFolderOpen /> : <FaFolder />} {node.name}
           </span>
           {isOpen &&
             node.children?.map((child) => (
-              <FileNode key={child.id} node={child} isChild={true} />
+              <FileNode
+                key={child.id}
+                node={child}
+                isChild={true}
+                setActiveTree={setActiveTree}
+              />
             ))}
         </div>
       ) : (
